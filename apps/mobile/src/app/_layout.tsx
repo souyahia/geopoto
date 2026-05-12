adc222e918d229903c5d8e67c2649dc77fc8c5cf
Samy Ouyahia <samy.ouyahia@mistral.ai>
2026-05-12 10:35:11 +0200

    Add HeroUI lib

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -2,7 +2,15 @@ import "../../global.css";
 import "@/services/i18n/i18n";
 
 import { Slot } from "expo-router";
+import { HeroUINativeProvider } from "heroui-native/provider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <Slot />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  );
 }
